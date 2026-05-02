76497c2c7f92bf554abfec835780135fca265027
Fredmaster08 <frxdmaster@gmail.com>
2026-05-02 18:11:45 +0200

    fixed shifting

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 
 #include <stdio.h> 
 #include <ctype.h>
+#include <string.h>
 
 int main() {
 
     char alpha[26];
+    char word[64];
     int x;
+    
+    printf("Enter a word: ");
+    fgets(word, sizeof(word), stdin);
+    printf("%s\n", word);
+    if (word < 65 || word > 90) {
+        printf("Invalid input\n");
+    }
 
     printf("How much do you want to shift? Number: ");
     scanf("%d", &x);
@@ -13,7 +22,13 @@ int main() {
     for (int i = 0; i < 26; i++) {
         alpha[i] = 'A' + i;
         alpha[i] = alpha[i] + x;
-        printf("%c\n", alpha[i]);
+        if (alpha[i] > 'Z') {
+            alpha[i] = 'A' + (alpha[i] - 91);
+        }
+        else if (alpha[i] < 'A') {
+            alpha[i] = 'Z' + (alpha[i] - 64);
+        }
+        printf("%d: %c\n", i, alpha[i]);
     }
 
        
