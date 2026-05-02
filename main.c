6304936a426a4c25dc2eb7d3b4da61bfd4d71b91
Fredmaster08 <frxdmaster@gmail.com>
2026-05-02 15:00:16 +0200

    shifting kinda works

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 
 #include <stdio.h> 
+#include <ctype.h>
 
 int main() {
 
-    printf("hello World\n");
+    char alpha[26];
+    int x;
+
+    printf("How much do you want to shift? Number: ");
+    scanf("%d", &x);
+
+    for (int i = 0; i < 26; i++) {
+        alpha[i] = 'A' + i;
+        alpha[i] = alpha[i] + x;
+        printf("%c\n", alpha[i]);
+    }
+
+       
 
     return 0;
 }
